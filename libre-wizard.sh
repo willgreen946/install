ec40df483da54339c52fed5cd3f802c943728de9
ZK <72352225+africavoid@users.noreply.github.com>
2023-05-14 09:47:30 +0000

    Added some new programs

--- libre-wizard.sh
+++ libre-wizard.sh
@@ -11,7 +11,7 @@ DOASDIR="/usr/local/etc/doas.conf"
 PKG="pkg install -y"
 
 # Change these to packages you want installed
-PKGS="drm-kmod mixertui wlroots evdev-proto libinput pkgconf neovim vifm zathura zathura-pdf-mupdf tuifeed htop wayland seatd foot wget doas curl librewolf"  
+PKGS="drm-kmod mixertui chafa neofetch wlroots evdev-proto libinput pkgconf neovim vifm zathura zathura-pdf-mupdf tuifeed htop wayland seatd foot wget doas curl librewolf"  
 
 # Dotfiles
 exec git clone https://github.com/africavoid/dwl &
