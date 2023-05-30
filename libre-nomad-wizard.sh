cfd97cad42a712b54388540febf46a36b2e1f187
ZK <72352225+africavoid@users.noreply.github.com>
2023-05-30 18:03:45 +0100

    Untested

--- libre-nomad-wizard.sh
+++ libre-nomad-wizard.sh
@@ -8,25 +8,20 @@ PKG_MAN_VOID="xbps-install"
 PKG_LIST_UNI="xorg feh lynx mpv zathura zathura-pdf-mupdf qutebrowser vim vifm chafa neofetch bash"
 PKG_LIST_VAR="curl wget"
 PKG_LIST_FREEBSD=""
+GIT="git clone https://github.com/africavoid/"
 
-exec chsc -s bash will & # Set bash as default shell
+# make dirs
+mkdir $HOME/Programs
+mkdir $HOME/Media
+mkdir $HOME/Media/Music
+mkdir $HOME/Media/Movies
 
-# git repos
-exec git clone https://github.com/africavoid/dwm &
-exec git clone https://github.com/africavoid/dwl &
-exec git clone https://github.com/africavoid/dots &
-exec git clone https://github.com/africavoid/slstatus &
-exec git clone https://github.com/africavoid/st &
-
-# installing software
-exec $PKG_MAN_OPENBSD $PKG_LIST_UNI &
-
-# Making folders
-exec mkdir $HOME/Programs &
-exec mkdir $HOME/Media &
-exec mkdir $HOME/Media/Music &
-exec mkdir $HOME/Media/Movies &
-exec mkdir $HOME/Downloads & 
+#exec chsc -s bash will & # Set bash as default shell
+exec $GIT\dwm &
+exec $GIT\dmenu &
+exec $GIT\tabbed &
+exec $GIT\st &
+exec $GIT\slstatus &
 
 exec cp -r dots/.config  $HOME/.config &
 exec cp -r dots/.fonts $HOME/.fonts &
